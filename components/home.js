94845b1565775c9871f89f68addabd133d76cf1f
Aman Khandelwal <aman.khandelwal@91trucks.com>
2023-06-19 17:05:11 +0530

    adding changes to live streaming

--- components/home.js
+++ components/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import CameraImage from "../assests/security_camera.png";
 // import { FiPlus } from "react-icons/Fi";
 import Cam from "./cam";
+import LiveStream from "./LiveStream";
 
 const Homee = ({ setVisible }) => {
 
@@ -26,7 +27,8 @@ const Homee = ({ setVisible }) => {
       >
         {/* <FiPlus className="text-2xl" /> */}
       </div>
-      <Cam />
+      {/* <Cam /> */}
+      <LiveStream />
     </div>
   );
 };
