778d71df2493d7ca955d23cb2bed5e197f46ff8e
EZEEJAIN <jezeejain123455@gmail.com>
2023-06-20 10:37:40 +0530

    changes in LiveStream.js

--- components/LiveStream.js
+++ components/LiveStream.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useRef } from 'react';
+import Hls from 'hls.js';
 
 const LiveStream = () => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    const eventSource = new EventSource('/api/stream-events');
+    let hls;
 
-    eventSource.onmessage = (event) => {
-      const streamData = event.data;
-
-      if (videoRef.current) {
-        videoRef.current.src = streamData;
-        videoRef.current.play();
+    const startStream = () => {
+      if (Hls.isSupported()) {
+        hls = new Hls();
+        hls.loadSource('/api/stream.m3u8');
+        hls.attachMedia(videoRef.current);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          videoRef.current.play();
+        });
+      } else if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+        videoRef.current.src = '/api/stream.m3u8';
+        videoRef.current.addEventListener('loadedmetadata', () => {
+          videoRef.current.play();
+        });
       }
     };
 
+    startStream();
+
     return () => {
-      eventSource.close();
+      if (hls) {
+        hls.destroy();
+      }
     };
   }, []);
 
