ef3e3db3cf92abbf682bfddec71a4289bc0ca010
Aman Khandelwal <aman.khandelwal@91trucks.com>
2023-06-28 16:02:34 +0530

    handle camera stop on close stream

--- pages/api/stop.js
+++ pages/api/stop.js
@@ -5,19 +5,19 @@
 //         function killProcessOnPort(port) {
 //             const killCommand = process.platform === "win32" ? "taskkill" : "kill";
 //             const command = process.platform === "win32" ? `netstat -ano | findstr :${port}` : `lsof -i:${port}`;
-          
+
 //             const getProcessId = spawn(command, { shell: true });
-          
+
 //             let processId = "";
-          
+
 //             getProcessId.stdout.on("data", (data) => {
 //               processId += data.toString();
 //             });
-          
+
 //             getProcessId.on("close", () => {
 //               const regex = process.platform === "win32" ? /[\d]+$/ : /\s+([\d]+)\s/;
 //               const matches = processId.match(regex);
-          
+
 //               if (matches && matches[1]) {
 //                 const pid = matches[1];
 //                 console.log(`Process running on port ${port} has PID: ${pid}`);
@@ -28,25 +28,25 @@
 //               }
 //             });
 //           }
-          
+
 //           function killProcess(pid) {
 //             const killCommand = process.platform === "win32" ? "taskkill" : "kill";
 //             const killProcess = spawn(killCommand, [process.platform === "win32" ? "/F" : "-9", pid]);
-          
+
 //             killProcess.stdout.on("data", (data) => {
 //               console.log(`stdout: ${data}`);
 //             });
-          
+
 //             killProcess.stderr.on("data", (data) => {
 //               console.error(`stderr: ${data}`);
 //             });
-          
+
 //             killProcess.on("close", (code) => {
 //               console.log(`Child process exited with code ${code}`);
 //               res.status(200).json({ message: `Child process exited with code ${code}` });
 //             });
 //           }
-          
+
 //           // Example: Kill process running on port 3001
 //           killProcessOnPort(3001);      
 //     }
@@ -60,6 +60,7 @@ import { join } from 'path';
 
 const outputPath = "/home/aman/Desktop/workspace/ip_cameras/output";
 const hlsOutputPath = "/home/aman/Desktop/workspace/ip_cameras/hls";
+let captureInterval;
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
@@ -83,6 +84,9 @@ export default function handler(req, res) {
         console.log('HLS output directory cleared');
       }
     };
+
+    // Stop capture interval
+    clearInterval(captureInterval);
 
     stopFFmpegProcess();
     clearDirectories();
